1fd2cc2cc419e0dffbee5cdc0b480eac1b1dd724
Agustin Levental <agustinlevental@gmail.com>
2022-08-31 16:12:49 -0300

    filter

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default function App() {
- 
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
       .then((res) => res.json())
       .then((data) => setData(data));
-      
   }, []);
+
+  function isFromEarth() {
+    const tagsUpdated = data.filter(({ origin }) => origin === "Earth");
+    setData(tagsUpdated);
+    return data;
+  }
+
+  function isFromEverywhere() {
+    fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }
 
   return (
     <div>
@@ -30,31 +42,54 @@ export default function App() {
           Final Space characters
         </Typography>
         <Grid container spacing={3}>
-        
-        {data.map((character) => (
-          
-          <Grid item xs={12} sm={4} key={character.id}>
-          <Card
-            style={{
-              maxWidth: 345,
-              boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-              backgroundColor: "#fafafa",
-            }}
+          <br />
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
             
           >
-            <CardMedia component='img' style={{ height: "300px" }} image={character.img_url} />
-            <CardContent>
-              <Typography color="primaty" variant="h5">
-                {character.name}
-              </Typography>
-              <Typography color="textSecondary" variant="subtitle2">
-                {`from ${character.origin}`}
-              </Typography>
-            </CardContent>
-          </Card>
-          </Grid>
-        ))}
-        
+            <Button variant="contained" onClick={isFromEarth} color="success" >
+              From Earth
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={isFromEverywhere}
+              color="secondary"
+            >
+              From everywhere
+            </Button>
+          </Stack>
+          
+            
+          {data.map((character) => (
+            <Grid item xs={12} sm={4} key={character.id}>
+              <Card
+                style={{
+                  maxWidth: 345,
+                  boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+                  backgroundColor: "#fafafa",
+                  
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  style={{ height: "300px" }}
+                  image={character.img_url}
+                />
+                <CardContent>
+                  <Typography color="primaty" variant="h5">
+                    {character.name}
+                  </Typography>
+                  <Typography color="textSecondary" variant="subtitle2">
+                    {`from ${character.origin}`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </div>
